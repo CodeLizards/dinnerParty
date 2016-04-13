dfcd8f6ef6ffcaee4aa7f21fb314e715973c768b
CodeLizards <ebharris93@gmail.com>
2016-04-12 17:32:08 -0700

    working on request handlers

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -61,18 +61,14 @@ exports.getAllEvents = function(req, res){
 
 //get one event
 exports.getOneEvent = function(req, res){
+  var user = req.user;
 
+  db.User.findOne({id: req.user.id})
+    .then(function(user){
+      user.getEvents().
+    });
+};
 
-}
-
-
-//requests to join tables
-exports.addGuest = function(req, res, next){
- //add event to guest/guest to event in join table
- // console.log('ADDGUEST NEXT', next);
- // addGuest(req.body.event, req.body.user, next);
-
-}
 
 
 exports.getAllGuests = function(req, res){
@@ -85,7 +81,7 @@ exports.getAllGuests = function(req, res){
 
 res.send(GuestListData);
 // get all guests for specific event
-  db.Event.findOne(req.body)
+  db.Event.findOne(req.body.event)
     .then(function(event){
       event.getUsers().then(function(users){
         var userAttributes=[];
@@ -94,7 +90,7 @@ res.send(GuestListData);
             userAttributes.push(users[i].id);
           }
           //do something with userAttributes
-          // console.log(userAttributes);
+          console.log(users);
         }
       });
     });
@@ -102,11 +98,26 @@ res.send(GuestListData);
 
 };
 
-var addGuest = function(event, guest, next){
- // console.log('HELPER NEXT', event);
+
+var addGuest = function(req, res, next){
+ // console.log('HELPER NEXT', event);  
  // console.log(event);
- // db.User.findOne({name: guest}).then(function(guest){
- //   db.Event.findOne({name: event.name})
+ // db.User.findOne({name: req.body.guest}).then(function(guest){
+ //   db.Event.findOne({name: req.body.event})
+ //   .then(function(event){
+ //     console.log('EVENT', event);
+ //     event.addUser(guest);
+ //     next();
+ //   });
+ // });
+};
+
+
+var addGuest = function(req, res, next){
+ // console.log('HELPER NEXT', event);  
+ // console.log(event);
+ // db.User.findOne({name: req.body.guest}).then(function(guest){
+ //   db.Event.findOne({name: req.body.event})
  //   .then(function(event){
  //     console.log('EVENT', event);
  //     event.addUser(guest);
