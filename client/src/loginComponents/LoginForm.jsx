04bda626e6ae1b3961618c4fc45937b3573d1e31
CodeLizards <ebharris93@gmail.com>
2016-04-13 14:21:27 -0700

    Implemented login page with a button that links to google/callback. Files running on localserver not dev-server

--- client/src/loginComponents/LoginForm.jsx
+++ client/src/loginComponents/LoginForm.jsx
@@ -5,41 +5,38 @@ import Input from 'react-bootstrap/lib/Input';
 class LoginForm extends React.Component {
   constructor() {
     super();
-    this.state = {
-      userName: '',
-      password: '',
-      url: 'http://localhost:3000/login',
-    };
-  }
+}
 
-  handleUserNameChange(e) {
-    this.setState({ userName: e.target.value });
-  }
 
-  handlePasswordChange(e) {
-    this.setState({ password: e.target.value });
-  }
-  // TODO: refactor to use getValue() to get the current state
-  // see https://react-bootstrap.github.io/components.html#forms
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   // $.get('http://localhost:3000/auth/google', function(data){
+  //   //     console.log('error', err);
+  //   // });
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: 'http://localhost:3000/auth/google',
+  //     xhrFields: {
+  //       withCredentials: true
+  //     },
+
+  //     success: function(data){
+  //       console.log('SUCCESS', data);
+  //     }
+
+  //   });
+
+  // }
+
   render() {
     return (
       <div>
-        <form onSubmit={(event) => this.props.handleSubmit(event, this.state)}>
-          <Input id="userName"
-            onChange={this.handleUserNameChange.bind(this)}
-            type="text" value={this.state.userName}
-            placeholder="What's your name?"
-          />
-          <Input id="password"
-            type="text" onChange={this.handlePasswordChange.bind(this)}
-            value={this.state.password}
-            placeholder="Enter a password"
-          />
-          <ButtonInput bsStyle="success" type="submit" value="Login to DinnerParty" />
-        </form>
+        <button><a href="http://localhost:3000/auth/google">Login with Google</a></button>
+        // <button onClick={(event) => this.handleSubmit(event)} value='Login with google'>Login With Google</button>
       </div>
     );
   }
+
 }
 
 export default LoginForm;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,62 @@
 //Serve it up!
 var express = require('express');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var path = require('path');
+var cors = require('cors');
 var app = express();
 var port = 3000;
 var cors = require('cors');
+
+
+// CORS FOR CROSS ORIGIN REQUESTS
+// app.all('/*', function(req, res, next) {
+//   console.log('HEADERS');
+//     res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.set('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+//     res.set('Access-Control-Allow-Credentials', true);
+//     next();
+// });
+
+// app.use(function(req, res, next) {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
+
+// var defaultCorsHeaders = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10 // Seconds.
+// };
+// var headers = defaultCorsHeaders;
+// headers['Content-Type'] = 'application/json';
+
+// use it before all route definitions
+// app.use(cors({origin: 'http://localhost:3000'}));
+
+// app.use(methodOverride('X-HTTP-METHOD-Override'));
+// app.options('*', cors());
+// app.use(cors());
+// var whitelist = ['http://localhost:3000', 'https://accounts.google.com'];
+// var corsOptions = {
+//   origin: function(origin, callback){
+//     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+//     callback(null, originIsWhitelisted);
+//   }
+// };
 
 //PARSING
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //SERVE UP STATIC FILES
-app.use(express.static(path.join(__dirname + '/../client/src')));
+app.use(express.static(path.join(__dirname + '/../client/')));
+
 
 //CROSS ORIGINE REQUESTS
 app.use(cors());
@@ -28,6 +73,7 @@ var corsOptions = {
 require('./routes/config.js')(app, express);
 //TODO: further modularize code for routing use below line:
 // require('./routes/PartyRouter.js')(app, express);
+
 
 //ROUTING
 var partyRouter = require(path.join(__dirname + '/routes/PartyRouter.js'));
