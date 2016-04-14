51e5eabd6a3462da07f9e9cc7eecdcc21280e7b3
CodeLizards <ebharris93@gmail.com>
2016-04-13 22:40:42 -0700

    added link so that login button reroutes to googleAuth page

--- client/src/loginComponents/LoginForm.jsx
+++ client/src/loginComponents/LoginForm.jsx
@@ -5,34 +5,13 @@ import Input from 'react-bootstrap/lib/Input';
 class LoginForm extends React.Component {
   constructor() {
     super();
-}
+  }
 
-
-  // handleSubmit(event) {
-  //   event.preventDefault();
-  //   // $.get('http://localhost:3000/auth/google', function(data){
-  //   //     console.log('error', err);
-  //   // });
-  //   $.ajax({
-  //     type: 'GET',
-  //     url: 'http://localhost:3000/auth/google',
-  //     xhrFields: {
-  //       withCredentials: true
-  //     },
-
-  //     success: function(data){
-  //       console.log('SUCCESS', data);
-  //     }
-
-  //   });
-
-  // }
 
   render() {
     return (
       <div>
         <button><a href="http://localhost:3000/auth/google">Login with Google</a></button>
-        // <button onClick={(event) => this.handleSubmit(event)} value='Login with google'>Login With Google</button>
       </div>
     );
   }
